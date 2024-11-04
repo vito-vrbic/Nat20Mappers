8ee855dc6e90e0d004c0440c5a7d5affbb3c1be4
MaksHiz <maksimilijan.hizman@gmail.com>
2024-11-04 19:23:50 +0100

    Signup i Login - Update 1
    Rađeno na Login.jsx i Signup.jsx, također dodana slika za prikaz lozinke te style sheetovi za Login i Signup. Prva verzija sadržava verziju 1 dogovorenog dizajna. Izgled stranice je više manje implementiran, samo se moraju promijeniti neke sitnice ili dodati slike za verziju 2. Od same funkcionalnosti je implementirana mogućnost selektiranja dodatne opcije za poslovne korisnike kada pri registraciji unose ime organizacije te na prijavnoj stranici je omogućeno preusmjeravanje na registracijski page. U sljedećem ažuriranju će fokus biti na implementaciji same funkcionalnosti registracije i prijave u sustav.

--- src/Frontend/ttrpg-finder-frontend/src/pages/Login.jsx
+++ src/Frontend/ttrpg-finder-frontend/src/pages/Login.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
+import '../styles/Login.css'
+import showPass from '../assets/ShowPassword.png'
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <div className='LogInBox'> {/*contaier for the entire form*/}
+        <div className="Title">Log in</div>
+        <div className='inputs'> {/*container for all the input types*/}
+            <div className='input'>
+              <div className='inputTag'>Username</div>
+              <input type="name" id="user" placeholder='Enter username'></input>
+            </div>
+            <div className='input'>
+              <div className='inputTag'>Password</div>
+              <input type="password" id="Password" placeholder='Enter password'></input>
+              <img src={showPass}></img>
+            </div>
+        </div>
+        <div className='forgotPassword'>Forgot password?</div>
+        <div className='submit'>Log in</div>
+        <div className='gotoSignup'>No account? <Link to="/signup">Sign up</Link></div>
+      </div>
+    </>
   )
 }
 

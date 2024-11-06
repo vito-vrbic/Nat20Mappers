5143e45e012058cfd866e0bd7ce5ecc31f1a0756
VedranLekic <velekic@gmail.com>
2024-11-06 22:43:20 +0100

    Adding the spring skeleton

---
+++ src/TTRPGBE-1/src/main/java/com/ttrpg/Ttrpgbe1Application.java
@@ -0,0 +1,13 @@
+package com.ttrpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ttrpgbe1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ttrpgbe1Application.class, args);
+	}
+
+}
---
+++ src/TTRPGBE-1/src/test/java/com/ttrpg/Ttrpgbe1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttrpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ttrpgbe1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

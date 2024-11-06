7b643bd407904b714dc61a66b91befe1fb6d2cb3
VedranLekic <velekic@gmail.com>
2024-11-06 22:52:23 +0100

    Update Ttrpgbe1Application.java

--- src/TTRPGBE-1/src/main/java/com/ttrpg/Ttrpgbe1Application.java
+++ src/TTRPGBE-1/src/main/java/com/ttrpg/Ttrpgbe1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Ttrpgbe1Application {
-
+ /*coommmentststs*/
 	public static void main(String[] args) {
 		SpringApplication.run(Ttrpgbe1Application.class, args);
 	}

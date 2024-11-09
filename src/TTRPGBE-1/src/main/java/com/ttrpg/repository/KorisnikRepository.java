ed7fc396cb5576e4d43b54198cc11947af811bb2
VedranLekic <velekic@gmail.com>
2024-11-09 17:10:33 +0100

    repo

---
+++ src/TTRPGBE-1/src/main/java/com/ttrpg/repository/KorisnikRepository.java
@@ -0,0 +1,20 @@
+package com.ttrpg.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ttrpg.model.Korisnik;
+
+@Repository
+
+public interface KorisnikRepository extends JpaRepository <Korisnik,Long>{
+	
+	
+	List<Korisnik> findByUsername(String username);
+	List<Korisnik> findByEmailContaining(String email);
+	Optional<Korisnik> findByUsernameAndEmail(String username, String email);
+
+}

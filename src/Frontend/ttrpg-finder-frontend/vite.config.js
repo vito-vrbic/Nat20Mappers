4ada8a258d50c08b499e13dc052e823f8fe5a138
Vito Vrbić <vito.vrbic@fer.unizg.hr>
2025-01-24 17:52:22 +0100

    uredio vite

---
+++ src/Frontend/ttrpg-finder-frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', //Adresa mock backenda
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // Correct rewrite pattern
+      }
+    } 
+  },
+})
